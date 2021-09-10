7fe3796e591cf9b699447cf532429822bf6e90e6
Renan Germano <renan.soares@fulllab.com.br>
2021-09-10 00:26:16 -0300

    Adjusting `RemoteFeedLoader`s `load` function to do not invokes the `HTTPClient` completion for the get(from: completion:) function when the `RemoteFeedLoader` instance is no longer in memory.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,7 +20,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			guard let _ = self else { return }
 			switch result {
 			case let .success((data, response)):
 				guard response.statusCode == RemoteFeedLoader.successStatusCode else {
